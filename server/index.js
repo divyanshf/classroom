e0abf459d6317a0ecb82751a29eea7e2e17a1d40
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-08-27 19:06:50 +0530

    :lock: added domain for cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` });
 const express = require('express');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.use(
+    cors({
+        origin: process.env.DOMAIN,
+        optionsSuccessStatus: 200,
+    })
+);
 
 app.get('/', (req, res) => {
     res.send('Home');
